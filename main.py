53d11dd0fe2f2011ddaa6b25c459a70c807f49f3
Anthyme <90059514+derorsche@users.noreply.github.com>
2025-12-30 12:56:07 +0900

    Add simple logging support

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import json
+import os
+from logging import config, getLogger
+
+import dotenv
+
+
 def main():
-    print("Hello from papyrus!")
+    dotenv.load_dotenv()
+
+    with open("./log/logging_conf.json", "r", encoding="utf-8") as f:
+        config.dictConfig(json.load(f))
+
+    logger = getLogger(__name__)
+    logger.setLevel(os.getenv("LOG_LEVEL", "WARNING"))
+
+    logger.warning("This is a public service announcement!")
+    logger.debug("Debugging!")
 
 
 if __name__ == "__main__":
