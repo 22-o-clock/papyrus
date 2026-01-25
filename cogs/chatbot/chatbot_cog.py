b77fc55e88710b4f7be19a2f4b36c333ac357b8e
Anthyme <90059514+derorsche@users.noreply.github.com>
2026-01-26 01:51:51 +0900

    Split text generation logic into Drafter and Styler

--- cogs/chatbot/chatbot_cog.py
+++ cogs/chatbot/chatbot_cog.py
@@ -1,45 +1,56 @@
-import os
 from logging import getLogger
+from typing import Any
 
-from discord import Message, TextChannel, app_commands
+from discord import Message
 from discord.ext import commands
 from openai import AsyncOpenAI
 
-from .responses_api import fetch_chatgpt_output_text
+from .responses_api import DraftGenerator, ResponseStyler
 
 logger = getLogger(__name__)
-
-OPENAI_MODEL = os.getenv("RESPONSES_API_MODEL", "gpt-4.1")
 
 
 class ChatBot(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.client = AsyncOpenAI()
-        self.latest_response: str | None = None
-        self.is_responding = False
-        self.target_channel: TextChannel | None = None
+        self.draft_generator = DraftGenerator(self.client)
+        self.response_styler = ResponseStyler(self.client)
+        self.target_channel: int = 0
+        self.short_term_memory: list[Message] = []
 
     @commands.Cog.listener()
     async def on_message(self, message: Message) -> None:
-        if message.author.bot:
+        if message.channel.id == self.target_channel:
+            self.add_message_to_short_term_memory(message)
+
+        if self.bot.user is not None and message.author.id == self.bot.user.id:
             return
 
-        for user in message.mentions:
-            if self.bot.user is not None and user.id == self.bot.user.id:
-                break
-        else:
-            return
+        is_reply_required = await self.is_reply_required(message)
 
-        response_text, self.latest_response = await fetch_chatgpt_output_text(
-            self.client, message, previous_response_id=self.latest_response
-        )
+        if is_reply_required:  # TODO @se-anthyme: ロック処理を追加
+            response_text = await self.generate_text_response()
+            await message.reply(response_text)
 
-        await message.reply(response_text)
+    def add_message_to_short_term_memory(self, message: Message) -> None:
+        self.short_term_memory.append(message)
 
-    @app_commands.command()
-    async def summon(self, ctx) -> None:
-        pass
+    async def is_reply_required(self, message: Message) -> bool:
+        # TODO @se-anthyme: 判定ロジックを追加
+        return True
+
+    async def generate_text_response(self) -> str:
+        if self.bot.user is None:
+            raise TypeError
+
+        draft = await self.draft_generator.draft(self.bot.user.id, self.short_term_memory)
+        return await self.response_styler.style(draft)
+
+    @commands.hybrid_command()
+    async def summon(self, ctx: commands.Context[Any]) -> None:
+        await ctx.send("Hello!")
+        self.target_channel = ctx.channel.id
 
 
 async def setup(bot: commands.Bot) -> None:
--- cogs/chatbot/responses_api.py
+++ cogs/chatbot/responses_api.py
@@ -1,15 +1,18 @@
+import datetime
 import os
 from logging import getLogger
 from typing import Any
 
+import dateutil
 from discord import Message
 from openai import AsyncOpenAI
 
-from .prompt import INSTRUCTIONS
+from .prompt import draft_generator_prompt, response_styler_prompt
 
 logger = getLogger(__name__)
 
-OPENAI_MODEL = os.getenv("RESPONSES_API_MODEL", "gpt-4.1")
+OPENAI_MODEL = os.getenv("RESPONSES_API_MODEL", "gpt-5.2")
+LOCAL_TIMEZONE = dateutil.tz.gettz("Asia/Tokyo")
 
 
 def convert_message_to_chatgpt_input(message: Message) -> list[dict[str, Any]]:
@@ -31,39 +34,63 @@ def convert_message_to_chatgpt_input(message: Message) -> list[dict[str, Any]]:
             # Files with uncommon extensions (e.g., .jfif) may cause errors.
             # see https://platform.openai.com/docs/guides/images-vision
 
-            chatgpt_input[0]["content"].append(
-                {"type": "input_image", "image_url": attachment.url}
-            )
+            chatgpt_input[0]["content"].append({"type": "input_image", "image_url": attachment.url})
 
         if attachment.content_type == "application/pdf":
-            chatgpt_input[0]["content"].append(
-                {"type": "input_file", "file_url": attachment.url}
-            )
+            chatgpt_input[0]["content"].append({"type": "input_file", "file_url": attachment.url})
 
     return chatgpt_input
 
 
-async def fetch_chatgpt_output_text(
-    client: AsyncOpenAI, message: Message, previous_response_id: str | None = None
-) -> tuple[str, str]:
-    response = await client.responses.create(
-        input=convert_message_to_chatgpt_input(message),  # type: ignore
-        instructions=INSTRUCTIONS,
-        model=OPENAI_MODEL,
-        previous_response_id=previous_response_id,
-        temperature=0.9,
-        tools=[
-            {
-                "type": "web_search",
-                "user_location": {"type": "approximate", "country": "JP"},
-            },
-            {
-                "type": "code_interpreter",
-                "container": {"type": "auto"},
-            },
-        ],
-    )
+class DraftGenerator:
+    def __init__(self, client: AsyncOpenAI) -> None:
+        self.client = client
 
-    logger.info("Response fetched: %s", response)
+    async def draft(self, ai_id: int, short_term_memory: list[Message]) -> str:
+        api_response = await self.client.responses.create(
+            input=self.generate_prompt(ai_id, short_term_memory),
+            model=OPENAI_MODEL,
+            reasoning={"effort": "medium"},
+            tools=[
+                {
+                    "type": "web_search",
+                    "user_location": {"type": "approximate", "country": "JP"},
+                },
+                {
+                    "type": "code_interpreter",
+                    "container": {"type": "auto"},
+                },
+            ],
+        )
 
-    return response.output[-1].content[0].text, response.id  # type: ignore
+        logger.debug("API called: %s", api_response)
+        logger.debug("Draft: %s", api_response.output[-1].content[0].text)  # type: ignore
+        return api_response.output[-1].content[0].text  # type: ignore
+
+    def generate_prompt(self, ai_id: int, short_term_memory: list[Message]) -> str:
+        message_chain = ""
+
+        for m in short_term_memory:
+            if m.author.id == ai_id:
+                message_chain += f"- Papyrus ({m.created_at.astimezone(LOCAL_TIMEZONE)}) '{m.clean_content}'\n"
+            else:
+                message_chain += f"- User@{m.author.id} ({m.created_at.astimezone(LOCAL_TIMEZONE)}) '{m.clean_content}'\n"
+        message_chain += f"- Papyrus ({datetime.datetime.now(LOCAL_TIMEZONE)}) '●●'"
+
+        return draft_generator_prompt.DRAFT_PROMPT.format(message_chain)
+
+
+class ResponseStyler:
+    def __init__(self, client: AsyncOpenAI) -> None:
+        self.client = client
+
+    async def style(self, original_draft: str) -> str:
+        api_response = await self.client.responses.create(
+            input=self.generate_prompt(original_draft),
+            model=OPENAI_MODEL,
+        )
+        logger.debug("Styled output: %s", api_response.output[-1].content[0].text)  # type: ignore
+        return api_response.output[-1].content[0].text  # type: ignore
+
+    def generate_prompt(self, original_draft: str) -> str:
+        return response_styler_prompt.STYLE_PROMPT.format(original_draft)
